7ed49e56365ad17685e818ef82498d02f10a78a6
Zainab Khan <166662819+Zainabkhan007@users.noreply.github.com>
2024-12-18 11:14:13 -0800

    Update urls.py

--- admin_section/urls.py
+++ admin_section/urls.py
@@ -30,6 +30,8 @@ urlpatterns = [
     path("get_complete_menu/",views.get_complete_menu,name='get_complete_menu'),
     path("edit_menu/<int:id>/",views.edit_menu,name='edit_menu'),
     path("add_menu_item/",views.add_menu_item,name='add_menu_item'),
+    path("get_menu_items/",views.get_menu_items,name='get_menu_items'),
+    path('update_menu_items/<int:pk>/', views.update_menu_items, name='update_menu_items'),
 
     # View Registered Students
     path("view_students/",views.view_students,name='view_students'),
