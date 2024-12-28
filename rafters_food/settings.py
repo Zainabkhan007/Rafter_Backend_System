6196f0304c0b8f54df43b7093d76dc9a59d89d5e
Zainabkhan <freelancewriter3377@gmail.com>
2024-12-28 04:45:29 -0800

    update

--- rafters_food/settings.py
+++ rafters_food/settings.py
@@ -69,8 +69,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "https://rafters-web.vercel.app", 
    "https://www.raftersfoodservices.ie",
-   "http://localhost",  
-    "http://127.0.0.1",
+   "http://localhost:3000",  
+    
    
 ]
 CORS_ALLOW_HEADERS = [
