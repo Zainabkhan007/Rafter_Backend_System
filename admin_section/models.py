984fc84de247faec5a017dca60948f0d2676fb94
Zainab Khan <166662819+Zainabkhan007@users.noreply.github.com>
2025-02-27 12:21:36 -0800

    Update models.py

--- admin_section/models.py
+++ admin_section/models.py
@@ -144,22 +144,7 @@ class Menu(models.Model):
     def __str__(self):
         return f"{self.id} Menu:   {self.menu_day} {self.name} - {self.cycle_name}"
     
-    @property
-    def is_active(self): 
-     current_date = timezone.now()    
-   
-     if self.is_active_time and timezone.is_naive(self.is_active_time):
-         self.is_active_time = timezone.make_aware(self.is_active_time)
-   
-     if self.start_date and self.end_date:
-        if self.start_date <= current_date.date() <= self.end_date:
-            if self.is_active_time and self.is_active_time <= current_date:
-                return True
 
-     if self.is_active_time and self.is_active_time > current_date:
-        return False
-
-     return False
 
 
     
